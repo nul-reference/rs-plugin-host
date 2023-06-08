012612473811a727ce4df3056c7a862b1dcaf880
Underscore Nul <underscore.nul@proton.me>
2023-06-08 02:07:07 -0500

    Refactor Tag Registry
    Revert change to the HashMap's order in favor of adding a vector to look
    up IDs. This effectively acts as a speciallized bimap. The length of the
    vector is also used as the new ID, as removing mappings is not allowed.

--- host/src/tag_registry.rs
+++ host/src/tag_registry.rs
@@ -1,31 +1,27 @@
 use std::{
-    collections::HashMap,
     ffi::{c_char, CStr, CString},
     mem::MaybeUninit,
 };
 
-use once_cell::sync::Lazy;
+static mut REGISTRY: once_cell::sync::Lazy<RegistryState> =
+    once_cell::sync::Lazy::new(RegistryState::default);
 
 #[derive(Default)]
 struct RegistryState {
-    map: HashMap<usize, CString>,
-    next_id: usize,
+    string_map: std::collections::HashMap<CString, usize>,
+    id_vec: Vec<*const c_char>,
 }
-
-static mut REGISTRY: Lazy<RegistryState> = Lazy::new(RegistryState::default);
 
 #[no_mangle]
 pub unsafe extern "C" fn register_tag(tag_name: *const c_char) -> usize {
     let tag = CStr::from_ptr(tag_name).to_owned();
-    let mut id: MaybeUninit<usize> = MaybeUninit::uninit();
-
-    if lookup_tag(tag_name, id) {
-        id.assume_init()
+    if let Some(id) = REGISTRY.string_map.get(&tag) {
+        *id
     } else {
-        id.write(REGISTRY.next_id);
-        REGISTRY.next_id += 1;
-        REGISTRY.map.insert(id.assume_init(), tag);
-        id.assume_init()
+        let id = REGISTRY.id_vec.len();
+        REGISTRY.id_vec.push(tag.as_ptr());
+        REGISTRY.string_map.insert(tag, id);
+        id
     }
 }
 
@@ -35,8 +31,8 @@ pub unsafe extern "C" fn lookup_tag(
     mut tag_id: MaybeUninit<usize>,
 ) -> bool {
     let tag = CStr::from_ptr(tag_name).to_owned();
-    if let Some((k, _)) = REGISTRY.map.iter().find(|(_, v)| **v == tag) {
-        tag_id.write(*k);
+    if let Some(id) = REGISTRY.string_map.get(&tag) {
+        tag_id.write(*id);
         true
     } else {
         false
@@ -48,11 +44,10 @@ pub unsafe extern "C" fn get_tag_by_id(
     tag_id: usize,
     mut tag_name: MaybeUninit<*const c_char>,
 ) -> bool {
-    match REGISTRY.map.get(&tag_id) {
-        Some(tag) => {
-            tag_name.write(tag.as_c_str().as_ptr());
-            true
-        }
-        None => false,
+    if let Some(tag) = REGISTRY.id_vec.get(tag_id) {
+        tag_name.write(*tag);
+        true
+    } else {
+        false
     }
 }
